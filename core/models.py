19ae3a54b92dd8202c4950be90c23444e0cc6d07
Lila <djyasin.ly@gmail.com>
2022-04-04 11:13:08 -0400

    Set up debug toolbar, added user model, created super user, and made first migrations.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __repr__(self):
+        return f"<User username={self.username}>"
+
+    def __str__(self):
+        return self.username
